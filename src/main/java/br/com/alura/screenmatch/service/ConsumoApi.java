e6882ffda695de41fa79dd3d6f9eaa0837261026
gabriellterto <gabrielterto1002@gmail.com>
2024-07-10 15:55:50 -0300

    Adiciona service e classe ConsumoApi para integração com a API OMDB

--- src/main/java/br/com/alura/screenmatch/service/ConsumoApi.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsumoApi.java
@@ -7,7 +7,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsumoApi {
-    public String obterDaados(String endereco) {
+    public String obterDados(String endereco) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
